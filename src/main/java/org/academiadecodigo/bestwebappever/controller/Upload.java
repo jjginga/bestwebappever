3cb060fb7723027949f82f053665c72169b9fe2c
Joel José Ginga <jjginga@gmail.com>
2020-04-09 00:23:23 +0100

    upload files testing

---
+++ src/main/java/org/academiadecodigo/bestwebappever/controller/Upload.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.bestwebappever.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class Upload {
+
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public String submit(){
+        return "/customer/upload";
+    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public String submit(@RequestParam("file") MultipartFile file, ModelMap modelMap) {
+        modelMap.addAttribute("file", file);
+        return "fileUploadView";
+    }
+
+}
