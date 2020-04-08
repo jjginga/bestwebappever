27a3a6a7a13e0aebb50d08a44587457e26d0992c
João Matias <jpmatias93@hotmail.com>
2020-04-08 16:22:44 +0100

    added model package

---
+++ src/main/java/org/academiadecodigo/bestwebappever/persistence/model/Customer.java
@@ -0,0 +1,170 @@
+package org.academiadecodigo.bestwebappever.persistence.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
+public class Customer extends AbstractModel {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    @OneToMany(
+            // propagate changes on customer entity to account entities
+            cascade = {CascadeType.ALL},
+
+            // make sure to remove accounts if unlinked from customer
+            orphanRemoval = true,
+
+            // user customer foreign key on account table to establish
+            // the many-to-one relationship instead of a join table
+            mappedBy = "customer",
+
+            // fetch accounts from database together with user
+            fetch = FetchType.EAGER
+    )
+    private List<Specimen> specimens = new ArrayList<>();
+
+    /**
+     * Gets the first name of the customer
+     *
+     * @return the customer last name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the first name of the customer
+     *
+     * @param firstName the name to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets the last name of the customer
+     *
+     * @return the customer last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets the last name of the customer
+     *
+     * @param lastName the name to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets the email of the customer
+     *
+     * @return the customer email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the email of the customer
+     *
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the phone of the customer
+     *
+     * @return the customer phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * Sets the phone of the customer
+     *
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Gets the customer specimens
+     *
+     * @return the specimens
+     */
+    public List<Specimen> getSpecimens() {
+        return specimens;
+    }
+
+
+    /**
+     * Adds a new specimen to the customer
+     *
+     * @param specimen the account to add
+     */
+    public void addAccount(Specimen specimen) {
+        specimens.add(specimen);
+        specimen.setCustomer(this);
+    }
+
+    /**
+     * Removes an specimen from the customer
+     *
+     * @param specimen the specimen to remove
+     */
+    public void removeSpecimen(Specimen specimen) {
+        specimens.remove(specimen);
+        specimen.setCustomer(null);
+    }
+
+    /**
+     * Adds a new specimen to the customer
+     *
+     * @param specimen the specimen to add
+     */
+    public void addSpecimen(Specimen specimen) {
+        specimens.add(specimen);
+        specimen.setCustomer(this);
+    }
+
+    /**
+     * Removes a specimen from the customer
+     *
+     * @param specimen the specimen to remove
+     */
+    public void removeRecipient(Specimen specimen) {
+        specimens.remove(specimen);
+        specimen.setCustomer(null);
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    @Override
+    public String toString() {
+
+        // printing recipients with lazy loading
+        // and no session will cause issues
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", specimens=" + specimens +
+                "} " + super.toString();
+    }
+}
---
+++ src/main/java/org/academiadecodigo/bestwebappever/persistence/model/Specimen.java
@@ -0,0 +1,140 @@
+package org.academiadecodigo.bestwebappever.persistence.model;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specimen")
+public class Specimen extends AbstractModel {
+    private String name;
+    private String email;
+    private String phone;
+    private Integer specimenNumber;
+    private String description;
+
+    @ManyToOne
+    private Customer customer;
+
+    /**
+     * Gets the name of the recipient
+     *
+     * @return the recipient name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the recipient
+     *
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the email of the recipient
+     *
+     * @return the recipient email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the email of the recipient
+     *
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the phone of the recipient
+     *
+     * @return the recipient phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * Sets the phone of the recipient
+     *
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Gets the specimen number
+     *
+     * @return the specimen number
+     */
+    public Integer getSpecimenNumber() {
+        return specimenNumber;
+    }
+
+    /**
+     * Sets the specimen number
+     *
+     * @param specimenNumber the specimen number to set
+     */
+    public void setSpecimenNumber(Integer specimenNumber) {
+        this.specimenNumber = specimenNumber;
+    }
+
+    /**
+     * Gets the recipient description
+     *
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the recipient description
+     *
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets the customer
+     *
+     * @return the customer
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * Sets the customer
+     *
+     * @param customer the customer to set
+     */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Specimen{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", specimenNumber=" + specimenNumber +
+                ", description='" + description + '\'' +
+                "} " + super.toString();
+    }
+}
